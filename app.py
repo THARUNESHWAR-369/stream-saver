643609898c6da02c57c3371461a53c9816031438
THARUNESHWAR S <s.tharuneshwar.369@gmail.com>
2022-02-22 21:03:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,95 @@
+
+from fileinput import filename
+from flask import Flask, request
+from flask import render_template, redirect, url_for, send_file, send_from_directory, abort, safe_join
+from source.ytdownload import YT_DOWNLOAD
+import os
+
+DOWNLOAD_DIR = 'downloads'
+
+if not os.path.exists(DOWNLOAD_DIR):
+    os.mkdir(DOWNLOAD_DIR)
+
+
+app = Flask(__name__)
+
+yt_download = YT_DOWNLOAD()
+
+app.config['DOWNLOAD_DIR'] = DOWNLOAD_DIR
+
+@app.route('/', methods=['POST', "GET"])
+def home():
+
+    download_type = None
+    thumbnail_url = None
+    title = None
+    video_res = None
+    audio_abr = None
+    video_url = None   
+        
+    if request.method == "POST":
+        video_url = request.form.to_dict()['video-url']
+        try:
+            video_res, audio_abr, thumbnail_url, title = yt_download.getAudioAndVideoStreams(video_url=video_url)
+        
+            return render_template('home.html', context={
+                "data":
+                    {"video_res":video_res, 
+                    'audio_abr':audio_abr, 
+                    "video_url":video_url,
+                    "thumbnail_url":thumbnail_url,
+                    "title":title,
+                    "error":None
+                    }
+                    }
+                                )        
+        except Exception as e:
+            
+            return render_template('home.html', context={
+                "data":
+                    {"video_res":video_res, 
+                    'audio_abr':audio_abr, 
+                    "video_url":video_url,
+                    "thumbnail_url":thumbnail_url,
+                    "title":title,
+                    "error":"Provide vaild URL."                        
+                    }
+                    }
+                                )  
+            
+            
+    return render_template('home.html', context={
+        "data":
+            {"video_res":None,
+             "audio_abr":None, 
+             "video_url":None,
+             "thumbnail_url":None,
+             "title":None,
+             "error":None
+             }
+            }
+                           )
+    
+
+@app.route("/download_file", methods=["GET","POST"])
+def download_video():
+    
+    print('download_video')
+    try:
+        format = request.form.to_dict()['format']        
+        print(format)
+        download_type= yt_download.getDownloadType(format=format)
+        print(download_type)
+        
+        fname = yt_download.downloadVideo(download_type, format)   
+        print("fname: ",fname)  
+    
+        return send_file(fname, as_attachment=True)
+    except:
+        return "Video download failed!"
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
---
+++ source/ytdownload.py
@@ -0,0 +1,42 @@
+from pytube import YouTube
+
+class YT_DOWNLOAD:
+    def __init__(self):
+        
+        pass
+    
+    def getAudioAndVideoStreams(self, video_url):
+        
+        video, video_res = [], []
+        audio, audio_abr = [], []
+        
+        yt = YouTube(video_url)
+        
+        self.video = yt.streams.filter(type='video', progressive=True).order_by('resolution')
+        self.video_res = [stream.resolution for stream in self.video]
+        
+        self.audio = yt.streams.filter(type='audio').order_by('abr')
+        self.audio_abr = [stream.abr for stream in self.audio]
+        
+        return self.video_res, self.audio_abr, yt.thumbnail_url, yt.title
+
+    def getDownloadType(self, format):
+        return 'audio' if format.endswith('kbps') else 'video'
+    
+    def downloadVideo(self, download_type, format):
+        
+        print(download_type)
+        filename = None        
+    
+        if download_type == 'video':
+            download_path = self.video[self.video_res.index(format)].download(output_path="downloads")
+            print(download_path)
+            filename = download_path#.split('//')[-1]
+            print(filename)
+        if download_type == 'audio':
+            download_path = self.audio[self.audio_abr.index(format)].download("downloads")
+            filename = download_path.split('//')[-1]
+        
+        print("Filename: ",filename)
+        return filename
+    

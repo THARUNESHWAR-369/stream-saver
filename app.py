a27c1bda4b1df7dae32314ab44ffdc6baa05203b
THARUNESHWAR S <s.tharuneshwar.369@gmail.com>
2022-04-17 07:57:41 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -4,25 +4,20 @@ from flask import render_template
 from flask import redirect
 from flask import url_for
 from flask import session
+
 from pytube import YouTube
 
 import os
 from io import BytesIO
 
-DOWNLOAD_DIR = 'downloads'
-
-# create download directory (if not exists)
-if not os.path.exists(DOWNLOAD_DIR):
-    os.mkdir(DOWNLOAD_DIR)
+from app_utils import check_valid_url
+from app_utils import url_details
 
 # create app instance
 app = Flask(__name__)
 
 # secret key
 app.config['SECRET_KEY'] = "adksjfksjdfiomewo34j3o4jw3oaj4"
-
-# Initialize download directory
-app.config['DOWNLOAD_DIR'] = DOWNLOAD_DIR
 
 # methods
 methods = ['POST', "GET"]
@@ -31,21 +26,46 @@ methods = ['POST', "GET"]
 home_route = '/'
 download_file_route = '/download_file'
 
+
+@app.route("/", methods=methods)
+def home():
+    if request.method == "POST":
+        session['video_url'] = request.form['video-url']
+
+        is_valid_url = check_valid_url(session['video_url'] )
+
+        if is_valid_url == True :
+            url_data = url_details(url=session['video_url'])
+            if  url_data['status'] == True:
+                return render_template('index.html', error=None, data=url_data)
+        return render_template('index.html', error="Url not Found :)", data=None)
+
+
+    return render_template('index.html', error=None, data=None)
+
+"""
 # home page
 @app.route(home_route, methods=methods)
 def home():
     if request.method == "POST":
+        print("Form URL: ", request.form.get("video-url"))
         session['link'] = request.form.get("video-url")
+        print(session['link'], YouTube(session['link']).streams.get_highest_resolution())
         try:
             url = YouTube(session['link'])
-            url.check_availability()
+            print("URL: ",url)
+            #url.check_availability()
+            print("Url availability: ",url)
 
-            print(session['link'])
+            print("Url data: ",url.streams)
+
+            print("Youtube Video URL: ",session['link'])
         except Exception as e:
             print("[app error]: ",e)
             return render_template("home.html", url=None, error="url not found.")
         return render_template("home.html", url=url, error=None)
-    return render_template("home.html", url=None, error= None)
+    else:
+        return render_template("home.html", url=None, error= None)
 
 # download file page
 @app.route(download_file_route, methods=methods)
@@ -60,6 +80,8 @@ def download_file():
         buffer.seek(0)
         return send_file(buffer, as_attachment=True, download_name=video.default_filename)
     return redirect(url_for("home"))
+
+"""
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ app_utils.py
@@ -0,0 +1,119 @@
+import requests
+
+REQUEST_URL = "https://save-from.net/api/convert"
+
+def check_valid_url(url):
+    __r = requests.post(REQUEST_URL, params={"url":url}).json()
+    if 'meta' not in __r:
+        return False
+    return True
+    #return False if not __r['meta'] else True
+
+def get_watch_value(url):
+    return url.split('v=')[-1]
+
+def get_thumbnail_url(v):
+    return f"https://i.ytimg.com/vi/{v}/hqdefault.jpg"
+
+def get_video_title(r):
+    return r['meta']['title']
+
+def get_video_duration(r):
+    return r['meta']['duration']
+
+def get_video_quality(r):
+    return r['video_quality']
+
+def get_url(r, id):
+    try:
+        return r['url'][id]['info_url']
+    except:
+        return r['url'][id]['url']
+
+def get_name(r, id):
+    return r['url'][id]['name']
+
+def get_video_quality_(r, id):
+    return r['url'][id]['quality']
+
+def get_filesize(r, id):
+    try:
+        return r['url'][id]['filesize']
+    except:
+        return 'UNDEFINED'
+
+def get_attrs(r, id):
+    if r['url'][id]['attr'] == []:
+        return None
+    else:
+        if r['url'][id]['attr']['class'] == "":
+            return None
+        return r['url'][id]['attr']['class']
+
+
+def audio_data(r):
+    audio_download_data = []
+    video_quality = get_video_quality(r)
+    audio_r_len = len(r['url'][len(video_quality):])
+
+    for vq_id in range(audio_r_len):
+        audio_download_data.append(
+            [
+                get_url(r, vq_id),
+                get_name(r, vq_id),
+                get_video_quality_(r, vq_id),
+                get_filesize(r, vq_id),
+                get_attrs(r, vq_id)
+            ]
+        )
+
+    return audio_download_data
+
+
+def video_data(r):
+    video_download_data = []
+    video_quality = get_video_quality(r)
+
+    for vq_id in range(len(video_quality)):
+
+        video_download_data.append(
+            [
+                get_url(r, vq_id),
+                get_name(r, vq_id),
+                get_video_quality_(r, vq_id),
+                get_filesize(r, vq_id),
+                get_attrs(r, vq_id)
+            ]
+        )
+
+    return video_download_data
+
+def url_details(url):
+    v = get_watch_value(url=url)
+
+    PARAMS = {
+        "url":url
+    }
+
+    __r = requests.post(REQUEST_URL, params=PARAMS)
+
+    if __r.status_code == 200:
+
+        r = __r.json()
+
+        return {
+            "status" : True,
+            "v" : v,
+            "thumbnail_url" : get_thumbnail_url(v=v),
+            "title" : get_video_title(r),
+            "duration" : get_video_duration(r),
+            "url": url,
+            "video_data" : video_data(r),
+            "audio_data" : audio_data(r)
+        }
+
+    return {
+        "status" : False
+    }
+
+
